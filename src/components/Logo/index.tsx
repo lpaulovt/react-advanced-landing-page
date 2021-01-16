0914a5caf23f59d7d414fbc9e6999687d132d254
lpaulovt <silvavitorlopes711@gmail.com>
2021-01-16 14:33:41 -0300

    :hammer: Fixing image path :muscle: 1

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,7 +3,7 @@ import { LogoProps } from 'types/api'
 import * as S from './styles'
 
 const Logo = ({ alternativeText, url }: LogoProps) => (
-  <S.LogoWrapper src={url} alt={alternativeText} />
+  <S.LogoWrapper src={`http://localhost:1337${url}`} alt={alternativeText} />
 )
 
 export default Logo
