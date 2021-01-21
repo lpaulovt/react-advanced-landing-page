51d340414d047bf3efb33914c45d69e346f73fba
lpaulovt <silvavitorlopes711@gmail.com>
2021-01-20 21:52:56 -0300

    :hammer: Refactoring code list pricing box

--- src/components/PricingBox/index.tsx
+++ src/components/PricingBox/index.tsx
@@ -26,15 +26,15 @@ const PricingBox = ({
         <span>{numberInstallments}x de</span> R${priceInstallment}
       </S.DiscountPrice>
     </S.Prices>
-    <S.BenefitsList>
-      <S.BenefitsItem dangerouslySetInnerHTML={{ __html: benefits }} />
-    </S.BenefitsList>
+    <S.BenefitsList dangerouslySetInnerHTML={{ __html: benefits }} />
 
     <Button href={button.url} onClick={onClick} withPrice>
       <p>{button.label}</p>
       <div>
         <S.ButtonFullPrice>R${totalPrice}</S.ButtonFullPrice>
-        <S.ButtonDiscountPrice>R${discountPrice}</S.ButtonDiscountPrice>
+        <S.ButtonDiscountPrice>
+          R${numberInstallments * priceInstallment}
+        </S.ButtonDiscountPrice>
       </div>
     </Button>
   </S.Box>
